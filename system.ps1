998ee77ec5fbd5b5306ae4820b4244a3d417b327
Luiz Gustavo Lovato Lessa <126582104+Leagali@users.noreply.github.com>
2025-05-05 08:42:54 -0300

    Add files via upload

---
+++ system.ps1
@@ -0,0 +1,11 @@
+if ( -Not (Test-Path "venv")) {
+    py -m venv venv
+}
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+. .\venv\Scripts\Activate.ps1
+py -m pip install --upgrade pip
+
+
+#pip install -e .
+pip install invoke
