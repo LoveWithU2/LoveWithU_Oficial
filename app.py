93647ef9969527814df805fc77f0583f616b9bca
ArthurRocco <arthurbrocco@gmail.com>
2025-05-12 18:33:24 -0300

    app.db

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from app import app
 
+app = Flask(__name__, static_folder='static')
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ system.ps1
@@ -0,0 +1,12 @@
+if ( -Not (Test-Path "venv")) {
+    py -m venv venv
+}
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+. .\venv\Scripts\Activate.ps1
+py -m pip install --upgrade pip
+
+flask db init
+
+pip install -e .
+pip install invoke
