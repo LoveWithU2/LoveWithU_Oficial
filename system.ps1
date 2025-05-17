13aa5ad158667d2449e99b7ec6e4b2b5482ea049
Luiz Gustavo Lovato Lessa <126582104+Leagali@users.noreply.github.com>
2025-05-17 11:21:39 -0300

    Add files via upload

--- system.ps1
+++ system.ps1
@@ -10,16 +10,3 @@ New-Item -ItemType Directory -Path "static\uploads" -Force
 
 pip install -e .
 pip install invoke
-
-if ( -Not (Test-Path "venv")) {
-    py -m venv venv
-}
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
-. .\venv\Scripts\Activate.ps1
-py -m pip install --upgrade pip
-
-flask db init
-
-pip install -e .
-pip install invoke
